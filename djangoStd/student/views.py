576dfdf7cb5036c23a3da67773e778e62e60e00f
bhy304 <bhy0512@gmail.com>
2019-03-13 21:39:19 +0900

    #10 Student Update 기능 구현 #11 Student Delete 기능 구현 #12 Student Insert 기능 구현

--- djangoStd/student/views.py
+++ djangoStd/student/views.py
@@ -12,7 +12,10 @@ major_list = ListView.as_view(model=Major, template_name='student/majorlist.html
 major_detail = DetailView.as_view(model=Major, template_name='student/majordetail.html')
 major_new = CreateView.as_view(model=Major, template_name='student/majorform.html', fields='__all__')
 major_update = UpdateView.as_view(model=Major, template_name='student/majorform.html', fields='__all__')
-major_del = DeleteView.as_view(model=Major, template_name='student/major_confirm_delete.html', success_url='/student/')
+major_del = DeleteView.as_view(model=Major, template_name='student/major_confirm_delete.html', success_url='/student/major/list/')
 
 student_list = ListView.as_view(model=Student, template_name='student/studentlist.html')
 student_detail = DetailView.as_view(model=Student, template_name='student/studentdetail.html')
+student_new = CreateView.as_view(model=Student, template_name='student/studentform.html', fields='__all__')
+student_update = UpdateView.as_view(model=Student, template_name='student/studentform.html', fields='__all__')
+student_del = DeleteView.as_view(model=Student, template_name='student/student_confirm_delete.html', success_url='/student/std/list/')
