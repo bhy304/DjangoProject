3e3ddf095a781b867aaac74da2519e31b9327741
bhy304 <bhy0512@gmail.com>
2019-03-13 17:44:37 +0900

    #5 Major Update 기능 추가

--- djangoStd/student/urls.py
+++ djangoStd/student/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('<pk>/detail/', views.major_detail, name='detail'),
     path('new/', views.major_new, name='insert'),
     path('<pk>/delete/', views.major_del, name='del'),
-    # path('update/', views.majorlist, name='update'),
+    path('<pk>/update/', views.major_update, name='update'),
 ]
